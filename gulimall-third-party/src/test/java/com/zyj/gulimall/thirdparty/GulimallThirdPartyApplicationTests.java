ef0ccb8bcd88ed8755ed10e4d87436524c134a53
Zhenyj <65398814+Zhenyj@users.noreply.github.com>
2023-03-13 14:34:40 +0800

    Update GulimallThirdPartyApplicationTests.java

--- gulimall-third-party/src/test/java/com/zyj/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/zyj/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -27,10 +27,10 @@ class GulimallThirdPartyApplicationTests {
     @Test
     public void testUpload() throws FileNotFoundException {
         //// Endpoint以杭州为例，其它Region请按实际情况填写。
-        //String endpoint = "oss-cn-beijing.aliyuncs.com";
+        //String endpoint = "xxxxxxxxxxxxxxxxx";
         //// 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录RAM控制台创建RAM账号。
-        //String accessKeyId = "LTAI5t8UkdrDyhgif4V3Ui1L";
-        //String accessKeySecret = "1aedjRQld2T9e6Ar84jGMpra43zbb4";
+        //String accessKeyId = "xxxxxxxxxxxxxxxxx";
+        //String accessKeySecret = "xxxxxxxxxxxxxxxxx";
         //
         //// 创建OSSClient实例。
         //OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
